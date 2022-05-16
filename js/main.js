75df58e6764d7aa1ad6f831c5ece1242121ca1f0
Samuel10 <samuelsalcedoramirez@gmail.com>
2022-05-16 10:24:15 -0500

    trabajo

---
+++ js/main.js
@@ -0,0 +1,28 @@
+let opciones={
+    producto:' ',
+    categoria:' ',
+    precio:' ',
+    cantidad:' ',
+
+
+}
+
+
+
+
+
+let arregloProducto=[]
+
+function enviar(){
+    let produto=document.getElementById('producto').value;
+    // alert(`EL produto es ${produto}`)
+    arregloProducto.push(produto)
+
+
+//    for(i of arregloProducto){
+//        if(arregloProducto[i]=produto){
+//            produto==arregloProducto[i];
+//        }
+//    }
+
+}
